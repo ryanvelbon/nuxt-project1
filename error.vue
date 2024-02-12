5384b371b2c3051c2aaa84d05cb4f9e7631e5d37
Ryan <vellabonelloryan@gmail.com>
2024-02-12 20:32:04 +0700

    feat: custom error page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-screen bg-white flex flex-col items-center justify-center space-y-6">
+        <h2 class="text-7xl font-bold text-primary-600">{{ error.statusCode }}</h2>
+        <p class="text-gray-700 text-lg">{{ error.message }}</p>
+        <button class="btn btn-primary" @click="handleError">Go Home...</button>
+    </div>    
+</template>
+
+<script setup>
+defineProps({
+    error: Object
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<style scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,7 +1,12 @@
 <script setup>
 const { id } = useRoute().params
 const uri = 'https://fakestoreapi.com/products/' + id
+
 const { data: product } = await useFetch(uri, { key: id })
+
+if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
 </script>
 
 <template>
